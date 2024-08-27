515bd175a93a74deeeeba498131a7f1ccbad3243
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-08-27 21:36:38 +0530

    Created new Branch to tarck my EXcelR DSA Progress, and implemented a program to perform linear search in arrays.

---
+++ C++/linearSearchArray.cpp
@@ -0,0 +1,32 @@
+//Program to implement linear search in arrays.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 20, 3, 40, 50, 33, 45, 32, 69, 78, 100};
+    int search_element;
+    bool flag = false;
+
+    cout << "Enter the element to be searched: " << endl;
+    cin >> search_element;
+
+
+    //sizeof() function give the any object in the form of total no of bytes
+    //that the object occupies in the memory. Thus sizeof(arr) gives output 44
+    //as there are 11 int values in the array that occupies a total of 44 bytes(4 bytes each).
+    for (int i = 0; i < sizeof(arr) / 4; i++)
+    {
+        if (arr[i] == search_element)
+        {
+            flag = true;
+            cout << search_element << " found at the " << i + 1 << "th place and at " << i << "th index in the array." << endl;
+            break;
+        }
+    }
+
+    if (!flag)
+        cout << "Element not found in the array.";
+
+    return 0;
+}
